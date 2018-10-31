824f0577332ae440690aa53eb1a531241dced0f7
Ulrich Schmidt <support@ulicms.de>
2018-10-31 13:56:49 +0100

    typo fixed

--- src/content/modules/backup_manager/lang/en.php
+++ src/content/modules/backup_manager/lang/en.php
@@ -5,7 +5,7 @@ add_translation("actions", "Actions");
 add_translation("backup_type_database", "Database");
 add_translation("download", "Download");
 add_translation("put_site_into_maintenance_mode", "Put site into maintenance while the procedure.");
-add_translation("please_wait_dot_dot_dot", "Pleasw wait...");
+add_translation("please_wait_dot_dot_dot", "Please wait...");
 add_translation("restore_backup", "Restore Backup");
 add_translation("restore_backup_x", "Restore backup %dir%...");
 add_translation("wrong_password", "Wrong password");
